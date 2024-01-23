c5b7d1293596013f1af3c2c5f89f3e514bcc0777
Jonathan Hermam <jherman@outlook.com.br>
2024-01-23 11:18:42 -0300

    Added EDIT page and functions

---
+++ scripts/edit.js
@@ -0,0 +1,133 @@
+var collection = {id: "", name: "", color: "", urls: []}
+var urls = []
+
+window.onload = () => {
+  loadData();
+  loadListeners();
+}
+
+function loadData () {
+  chrome.storage.local.get(["theme"]).then((result) => {
+    pageMain = document.getElementById("edit-page");
+    collectionNameInput = document.getElementById("collection-name-input")
+    collectionColorInput = document.getElementById("collection-color-input")
+    newUrlInput = document.getElementById("new-url")
+    urlsArea = document.getElementById("urls-area")
+    back_icon = document.getElementById("back-icon")
+    save_icon = document.getElementById("save-icon")
+    icons = [back_icon]
+    for (icon of icons) {
+        icon.classList.value = `icon-${result.theme}`
+    }
+    pageMain.classList.add(`full-page-${result.theme}`);
+    collectionNameInput.classList.add(`input-${result.theme}`)
+    collectionColorInput.classList.add(`input-color-${result.theme}`)
+    urlsArea.classList.add(`border-${result.theme}`)
+    newUrlInput.classList.add(`input-${result.theme}`)
+    save_icon.classList.add(`icon-dark`)
+
+    chrome.storage.local.get(["collections"]).then((result) => {
+        const collectionId = window.location.search.split("=")[1];
+        const col = result.collections.find((col) => col.id = collectionId);
+        if (col) {
+            collection = col;
+            collectionColorInput.value = collection.color;
+            collectionNameInput.value = collection.name;
+            urls = collection.urls;
+            loadUrls();
+        } else {
+            alert(`Could not find collection ${collectionId}`);
+            navigation.back();
+        }
+    });
+  });
+}
+
+function loadListeners () {
+  var addingUrl = false;
+  const add_url_button = document.getElementById("add-url-button");
+  add_url_button.addEventListener("click", (event) => {addUrl()});
+  const newUrlInput = document.getElementById("new-url");
+  newUrlInput.addEventListener("focus", () => {
+    addingUrl = true;
+  });
+  newUrlInput.addEventListener("blur", () => {
+    addingUrl = false;
+  });
+  const collection_form = document.getElementById("edit-collection-form");
+  collection_form.addEventListener("submit", async (event) => {
+    if (addingUrl) {
+      event.preventDefault();
+      addUrl();
+    } else {
+      const nameInput = document.getElementById("collection-name-input")
+      if (nameInput.value == "" || urls.length == 0) {
+        event.preventDefault();
+        errors = []
+        if (nameInput.value == "") {
+          errors.push('The field "Name" is mandatory');
+        }
+        if (urls.length == 0) {
+          errors.push('You cannot edit a collection without any URLs');
+        }
+        alert(`Could not save the new collection due to these erros: ${errors.join('\n')}`);
+      } else {
+        const currentCollections = await chrome.storage.local.get(["collections"]);
+        const cols = currentCollections.collections || [];
+        collection.name = nameInput.value;
+        collection.color = document.getElementById("collection-color-input").value;
+        collection.urls = urls;
+        const index = cols.findIndex((col) => col.id = collection.id);
+        cols[index] = collection;
+        chrome.storage.local.set({ "collections": cols } );
+        navigation.back();
+      }
+    }
+  });
+}
+
+function loadUrls () {
+  urlsArea = document.getElementById("included-urls");
+  output = ""
+  for (let url of urls) {
+    output += `<div class="form-row">
+                <span>${maxString(url.string, "url")}</span>
+                <button class="icon-button" type="button" id="delete-url-${url.id}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 448 512" class="danger-icon"><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"/></svg>
+                </button>
+            </div>`
+  }
+  urlsArea.innerHTML = output;
+
+  for (let url of urls) {
+    delete_button = document.getElementById(`delete-url-${url.id}`);
+    delete_button.addEventListener("click", () => {
+        removeUrl(url);
+    });
+  }
+}
+
+function addUrl () {
+  newUrlInput = document.getElementById("new-url");
+  urls.push({ string: newUrlInput.value, id: `${collection.id}-${urls.length + 1}` })
+  newUrlInput.value = ""
+  loadUrls();
+}
+
+function removeUrl (url) {
+    urls.splice(urls.indexOf(url), 1);
+    loadUrls();
+}
+
+function maxString (value, type) {
+    switch (type) {
+        case "url":
+            if (value.length > 25) return value.substring(0, 23) + "...";
+            return value;
+        case "title":
+            if (value.length > 17) return value.substring(0, 14) + "...";
+            return value;
+        default:
+            return value;
+    }
+}
--- scripts/settings.js
+++ scripts/settings.js
@@ -120,14 +120,6 @@ function getUrls (item, theme) {
             canAdd = false;
         }
     }
-    if (canAdd == true) output += `
-                            <div class="input-add">
-                                <input type="text" id="txt-area-url-${item.id}" placeholder="Type the new url" class="input-${theme}"/>
-                                <button id="add-url-button-${item.id}" class="add-button" type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 448 512" class="icon-${theme}"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>
-                                </button>
-                            </div>
-                        `;
     return output;
 }
 
@@ -152,7 +144,7 @@ function getDefaultCollectionHeader (collection, collections, theme) {
             <span class="collection-name">${maxString(collection.name, "title")}</span>
         </div>
         <div class="collection-options">
-            <button id="collection-edit-${collection.id}" type="button" class="edit" title="Edit ${collection.name} collection">
+            <button id="collection-options-${collection.id}" type="button" class="edit" title="Edit ${collection.name} collection">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512" class="icon-${theme}"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
             </button>
             <button id="collection-delete-${collection.id}" type="button" class="delete" title="Delete ${collection.name} collection">
@@ -163,48 +155,7 @@ function getDefaultCollectionHeader (collection, collections, theme) {
     setCollectionActions(collection, collections, theme);
 }
 
-function getEditCollectionHeader (collection, collections, theme) {
-    isFirst = false;
-    isLast = false;
-    if (collections.indexOf(collection) == 0) {
-        isFirst = true;
-    }
-    if (collections.indexOf(collection) == (collections.length - 1)) {
-        isLast = true;
-    }
-    url_element = document.getElementById(`collection-header-${collection.id}`);
-    url_element.innerHTML = `
-        <div class="collection-info">
-            <div class="collection-move-options">
-                <button id="collection-move-down-${collection.id}" type="button" class="collection-edit-arrow" ${isLast ? 'disabled' : ''}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 448 512" class="option${isLast ? '-disabled' : ''}-icon"><path d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"/></svg>
-                </button>
-                <button id="collection-move-up-${collection.id}" type="button" class="collection-edit-arrow" ${isFirst ? 'disabled' : ''}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 448 512" class="option${isFirst ? '-disabled' : ''}-icon"><path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"/></svg>
-                </button>
-            </div>
-            <input class="input-color" type="color" value="${collection.color}" id="input-collection-color-edit-${collection.id}" title="Change collection color" />
-            <input class="input-collection-edit input-${theme}" type="text" value="${collection.name}" id="input-collection-edit-${collection.id}" />
-        </div>
-        <div class="collection-options">
-            <button id="collection-save-${collection.id}" type="button" class="save" title="Save ${collection.name} collection">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 448 512" class="icon-${theme}"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-            </button>
-            <button id="collection-cancel-${collection.id}" type="button" class="cancel" title="Cancel changes on ${collection.name} collection">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 384 512" class="danger-icon"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
-            </button>
-        </div>
-    `
-    setCollectionActions(collection, collections, theme);
-}
-
 function setCollectionActions (collection, collections, theme) {
-    edit_collection_button = document.getElementById(`collection-edit-${collection.id}`);
-    if (edit_collection_button) {
-        edit_collection_button.addEventListener("click", (event) => {
-            getEditCollectionHeader(collection, collections, theme);
-        });
-    }
     delete_collection_button = document.getElementById(`collection-delete-${collection.id}`);
     if (delete_collection_button) {
         delete_collection_button.addEventListener("click", (event) => {
@@ -212,40 +163,10 @@ function setCollectionActions (collection, collections, theme) {
             chrome.storage.local.set({ "collections": collections });
         });
     }
-    save_collection_button = document.getElementById(`collection-save-${collection.id}`);
-    if (save_collection_button) {
-        save_collection_button.addEventListener("click", (event) => {
-            collection.name = document.getElementById(`input-collection-edit-${collection.id}`).value;
-            collection.color = document.getElementById(`input-collection-color-edit-${collection.id}`).value;
-            getDefaultCollectionHeader(collection, collections, theme);
-            chrome.storage.local.set({ "collections": collections });
-        });
-    }
-    cancel_collection_button = document.getElementById(`collection-cancel-${collection.id}`);
-    if (cancel_collection_button) {
-        cancel_collection_button.addEventListener("click", (event) => {
-            getDefaultCollectionHeader(collection, collections, theme);
-            chrome.storage.local.set({ "collections": collections });
-        });
-    }
-    move_up_collection_button = document.getElementById(`collection-move-up-${collection.id}`)
-    if (move_up_collection_button) {
-        move_up_collection_button.addEventListener("click", (event) => {
-            current_index = collections.indexOf(collection);
-            element_before = collections[current_index - 1]
-            collections[current_index - 1] = collection
-            collections[current_index] = element_before
-            chrome.storage.local.set({ "collections": collections });
-        });
-    }
-    move_down_collection_button = document.getElementById(`collection-move-down-${collection.id}`)
-    if (move_down_collection_button) {
-        move_down_collection_button.addEventListener("click", (event) => {
-            current_index = collections.indexOf(collection);
-            element_after = collections[current_index + 1]
-            collections[current_index + 1] = collection
-            collections[current_index] = element_after
-            chrome.storage.local.set({ "collections": collections });
+    options_collection_button = document.getElementById(`collection-options-${collection.id}`)
+    if (options_collection_button) {
+        options_collection_button.addEventListener("click", () => {
+            window.location.assign(`edit.html?collection=${collection.id}`);
         });
     }
 }
@@ -259,9 +180,6 @@ function getDefaultUrlElement (url, item, collections, theme) {
         <div class="url-options">
             <button class="edit" type="button" id="edit-url-button-${url.id}" title="Edit ${url.string} tab from ${item.name} collection">
                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 512 512" class="icon-${theme}"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
-            </button>
-            <button class="delete" type="button" id="delete-url-button-${url.id}" title="Remove ${url.string} tab from ${item.name} collection">
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 448 512" class="danger-icon"><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"/></svg>
             </button>
         </div>
     `
@@ -338,13 +256,6 @@ function setUrlButtonsActions (url, item, collections, theme) {
             
             item.urls.splice(selected_element_index, 1);
             item.urls.splice(target_element_index < selected_element_index ? target_element_index - 1 : target_element_index, 0, selected_element);
-            chrome.storage.local.set({ "collections": collections });
-        });
-    }
-    delete_button = document.getElementById(`delete-url-button-${url.id}`);
-    if (delete_button) {
-        delete_button.addEventListener("click", (event) => {
-            item.urls.splice(item.urls.indexOf(url), 1);
             chrome.storage.local.set({ "collections": collections });
         });
     }
