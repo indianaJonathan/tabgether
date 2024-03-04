b909122be9d01b807c7c4ff6de0e14e4c07783e9
Jonathan Hermam <jherman@outlook.com.br>
2024-03-04 17:11:02 -0300

    fix: Updated Export script to match project patterns

--- scripts/export.js
+++ scripts/export.js
@@ -1,121 +1,153 @@
 const collections_to_export = [];
-let lang;
+let locale;
+let collections = [];
 
-window.onload = () => {
-    getLocation();
+window.onload = async () => {
+    // Get language from storage
+    const result = await chrome.storage.local.get(["language", "collections"]);
+    const selectedLang = result.language || "en-us";
+    // Get locale from file
+    locale = await fetch(`../languages/${selectedLang}.json`)
+        .then((response) => response.json());
+
+    const collections = result.collections || [];
+
+    await loadData();
+    loadListeners(collections);
+    applyLocation(collections);
+    applyTheme(collections);
     chrome.storage.onChanged.addListener(() => {
-        getLocation();
+        loadData();
+        loadListeners(collections);
+        applyLocation(collections);
+        applyTheme(collections);
     });
 }
 
-function getLocation() {
-    chrome.storage.local.get(["language", "theme"]).then(async (result) => {
-        selectedLang = "en-us";
-        if (result.language) {
-            selectedLang = result.language;
-        } else {
-            chrome.storage.local.set({"language": "en-us"});
-        }
-        lang = await fetch(`../languages/${selectedLang}.json`)
-            .then((response) => response.json());
-        getCollections();
-    });
-}
+async function applyLocation(collections) {
+    // Get page elements
+    const pageTitle = document.getElementById("page-title");
+    const exportButton = document.getElementById("export-collections-button");
 
-function getCollections () {
-    chrome.storage.local.get(["collections", "theme"]).then((result) => {
-        pageMain = document.getElementById("export-page");
-        pageMain.classList.value = `full-page-${result.theme}`
-        pageTitle = document.getElementById("page-title");
-        pageTitle.innerHTML = lang.titles.export_collections;
-        container = document.getElementById("container")
-        container.classList.value = `container-${result.theme}`
-        back_icon = document.getElementById("back-icon")
-        icons = [back_icon]
-        for (icon of icons) {
-            icon.classList.value = `icon-${result.theme}`
-        }
-        container.innerHTML = `<span>No collections found</span>`;
-        if (result.collections && result.collections.length > 0) {
-            container.innerHTML = `
-                <form id="export-collections-form" class="export-form">
-                </form>
-            `;
-            getCollectionsElements(result.collections, result.theme);
-            form = document.getElementById("export-collections-form")
-            form.addEventListener("submit", (event) => {
-                event.preventDefault();
-                downloadObjectAsJson(collections_to_export, "tab_collections")
-            });
-        }
-    });
-}
-
-function getCollectionsElements (collections, theme) {
-    exportForm = document.getElementById("export-collections-form");
-    output = "";
-    for (let collection of collections) {
-        output += `
-            <div class="border-${theme}" id="collection-select-${collection.id}" title="${lang.others.captions.select_collection}">
-                <div class="tabs-collection" id="collection-export-${collection.id}">
-                    <input type="checkbox" id="checkbox-${collection.id}" name="checkbox-${collection.id}" hidden/>
-                    <span class="collection-name">${collection.name}</span>
-                </div>
-            </div>
-        `
+    // Apply locale to elements
+    pageTitle.innerHTML = locale.titles.export_collections;
+    exportButton.innerHTML = locale.buttons.captions.export;
+    if (exportButton) {
+        const shouldDisable = collections_to_export.length === 0;
+        exportButton.disabled = shouldDisable;
+        exportButton.title = shouldDisable ? locale.others.errors.no_collection_selected : "";
+        exportButton.innerHTML = locale.buttons.captions.export;
     }
-    output += `<div class="form-buttons" id="form-buttons"></div>`
-    exportForm.innerHTML = output;
-    getFormButtons();
-    setActions(collections, theme);
+
+    // Apply locale to collections elements
+    for (const collection of collections) {
+        const collectionComponent = document.getElementById(`collection-select-${collection.id}`);
+        const isSelected = collections_to_export.includes(collection);
+        if (collectionComponent) collectionComponent.title = !isSelected ? locale.others.captions.select_collection : locale.others.captions.deselect_collection;
+    }
 }
 
-function setActions (collections, theme) {
+async function applyTheme(collections) {
+    // Get theme from storage
+    const result = await chrome.storage.local.get(["theme"]);
+    const theme = result.theme || "dark";
+
+    // Get elements from page
+    const pageMain = document.getElementById("export-page");
+    const container = document.getElementById("container");
+    const exportButton = document.getElementById("export-collections-button");
+    const backIcon = document.getElementById("back-icon");
+    const icons = [backIcon];
+
+    const shouldDisable = collections_to_export.length === 0;
+
+    // Appy theme on elements
+    pageMain.classList.value = `full-page-${theme}`;
+    container.classList.value = `container-${theme}`;
+    exportButton.classList.value = shouldDisable ? "export-button-disabled" : "export-button";
+
+    for (const icon of icons) {
+        icon.classList.value = `icon-${result.theme}`
+    }
+
+    // Apply theme for collections components
+    for (const collection of collections) {
+        const collectionComponent = document.getElementById(`collection-select-${collection.id}`);
+        const isSelected = collections_to_export.includes(collection);
+        if (collectionComponent) collectionComponent.classList.value = `border-${theme}`;
+        if (isSelected) collectionComponent.classList.value += ` selected-${theme}`;
+    }
+}
+
+async function loadData() {
+    // Get data from storage
+    const result = await chrome.storage.local.get(["collections"]);
+    const collections = result.collections || [];
+
+    // Insert data into page container
+    const container = document.getElementById("container");
+    if (collections.length > 0) {
+        container.innerHTML = `
+            <form id="export-collections-form" class="export-form">
+                ${collections.map((collection) => {
+            return CollectionComponent(collection);
+        }).join("")}
+                <div class="form-buttons" id="form-buttons">
+                    <button id="export-collections-button" type="submit"></button>
+                </div>
+            </form>
+        `;
+    } else {
+        container.innerHTML = `<span>No collections found</span>`;
+    }
+}
+
+function loadListeners(collections) {
+    const form = document.getElementById("export-collections-form");
+    if (form) form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        downloadObjectAsJson(collections_to_export, "tab_collections")
+    });
+
     for (let collection of collections) {
-        element = document.getElementById(`collection-select-${collection.id}`);
-        element.addEventListener("click", (event) => {
-            checkbox = document.getElementById(`checkbox-${collection.id}`);
+        const element = document.getElementById(`collection-select-${collection.id}`);
+        const checkbox = document.getElementById(`checkbox-${collection.id}`);
+        element.addEventListener("click", () => {
             checkbox.click();
-            getFormButtons();
+            applyTheme(collections);
+            applyLocation(collections);
         });
-        checkbox = document.getElementById(`checkbox-${collection.id}`);
-        checkbox.addEventListener("click", (event) => {
-            collectionDiv = document.getElementById(`collection-select-${collection.id}`);
+        checkbox.addEventListener("click", () => {
+            const collectionDiv = document.getElementById(`collection-select-${collection.id}`);
             if (checkbox.checked) {
-                collectionDiv.classList.value = `border-${theme} selected-${theme}`;
-                collectionDiv.title = `${lang.others.captions.deselect_collection}`;
+                collectionDiv.title = `${locale.others.captions.deselect_collection}`;
                 collections_to_export.push(collection);
                 collections_to_export.sort(function (a, b) { return a.id - b.id });
             } else {
-                collectionDiv.classList.value = `border-${theme}`;
-                collectionDiv.title = `${lang.others.captions.select_collection}`;
+                collectionDiv.title = `${locale.others.captions.select_collection}`;
                 collections_to_export.splice(collections_to_export.indexOf(collection), 1);
             }
         });
     }
 }
 
-function getFormButtons() {
-    buttons = document.getElementById("form-buttons");
-    buttons.innerHTML = collections_to_export.length > 0 ? `
-            <button id="export-collections-button" class="export-button" type="submit">
-                ${lang.buttons.captions.export}
-            </button>
-        `
-    :
-        `
-            <button id="export-collections-button-disabled" class="export-button-disabled" type="submit" title="${lang.others.errors.no_collection_selected}" disabled>
-                ${lang.buttons.captions.export}
-            </button>
-        `
-}
-
-function downloadObjectAsJson(exportObj, exportName){
+function downloadObjectAsJson(exportObj, exportName) {
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
     var downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("href", dataStr);
     downloadAnchorNode.setAttribute("download", exportName + ".json");
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
+}
+
+function CollectionComponent(collection) {
+    return `
+        <div id="collection-select-${collection.id}">
+            <div class="tabs-collection" id="collection-export-${collection.id}">
+                <input type="checkbox" id="checkbox-${collection.id}" name="checkbox-${collection.id}" hidden/>
+                <span class="collection-name">${collection.name}</span>
+            </div>
+        </div>
+    `
 }
